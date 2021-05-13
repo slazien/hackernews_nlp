6ce33ff24f368c3bd8d85d70dfa0607fc072238d
slazien <p.zientala.1995@gmail.com>
2021-05-13 13:17:17 +0200

    added prelim code for training word2vec and better parallel text processing

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -1,23 +1,23 @@
+import re
 from multiprocessing import Pool, cpu_count
-from typing import Optional
+from typing import Iterable, Optional
 
 from tqdm import tqdm
 
 from src.preprocessing.text import *
 
-NUM_CORES = cpu_count() - 1
+NUM_CORES = cpu_count() - 1 or 1
 
 
 class TextPreprocessor:
-    def __init__(self, text_list: List[str], to_lowercase: bool = True):
-        self.text_list = text_list
+    def __init__(self, to_lowercase: bool = True):
         self.to_lowercase = to_lowercase
 
     def process(self, text: str) -> Optional[str]:
         """
         Process a single string by applying cleaning methods defined in text.py
         :param text: string to process
-        :return: processed string, None if supplied string was None
+        :return: processed string, None if supplied or output string is None or if returned text would be empty
         """
         if text is None:
             return None
@@ -27,16 +27,56 @@ class TextPreprocessor:
         if self.to_lowercase:
             text = lowercase(text)
         text = remove_nonalphanumeric(text)
+        text = re.sub(" +", " ", text)
+        text = text.strip()
+
+        if text is None or text == "":
+            return None
 
         return text
 
-    def process_multiprocessing(self) -> List[Optional[str]]:
+    def process_multiprocessing(
+        self,
+        texts: Iterable[str],
+        file_name: str,
+        total_count: int,
+        batch_size: int = 10000,
+    ):
         """
-        Parallelize the process method using n - 1 CPU cores
-        :return: list of processed strings
+        Parallelize the process method using n - 1 CPU cores and save results in batches to a file
+        :param texts: an iterable with string objects
+        :param file_name: path to the file to append preprocessed texts to
+        :param total_count: total number of records to iterate over
+        :param batch_size: the size of a single text list to use for parallel preprocessing
+        :return:
         """
-        with Pool(NUM_CORES) as pool:
-            text_processed = list(
-                tqdm(pool.imap(self.process, self.text_list), total=len(self.text_list))
-            )
-        return text_processed
+
+        num_processed = 0
+
+        with tqdm(total=total_count) as pbar:
+            with open(file_name, "a") as f:
+                with Pool(NUM_CORES) as pool:
+                    while True:
+                        # Create a batch of texts and cast it to a list for use in pool.imap
+                        current_batch = []
+                        for i in range(batch_size):
+                            current_batch.append(next(texts))
+
+                        # Break the loop if no more texts to process
+                        if len(current_batch) == 0:
+                            break
+
+                        # Process texts in parallel
+                        text_processed_batch = list(
+                            tqdm(
+                                pool.imap(self.process, current_batch),
+                                total=total_count,
+                            )
+                        )
+
+                        # Save processed texts to a file, with each text in a new line
+                        for text in text_processed_batch:
+                            if text is not None:
+                                f.write(text + "\n")
+                            num_processed += 1
+                            pbar.update(num_processed)
