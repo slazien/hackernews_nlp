fcd112b367d7ca6eb4d2f186acedf028f1b98916
slazien <p.zientala.1995@gmail.com>
2021-05-16 17:37:46 +0200

    fixes to luigi tasks

--- src/utils/list.py
+++ src/utils/list.py
@@ -2,6 +2,12 @@ from typing import List
 
 
 def chunk_for_size(input_list: List, chunk_size: int) -> List[List]:
+    """
+    Returns consecutive sublists of size chunk_size
+    :param input_list: list to chunk
+    :param chunk_size: size of each sublist
+    :return: list of lists (chunks)
+    """
     for x in range(0, len(input_list), chunk_size):
         chunk = input_list[x : chunk_size + x]
 
